bafb54ac4be9274a581e69895db01b630a756448
Eliezer Silva <eliezerliz2704@gmail.com>
2023-08-15 10:45:01 -0300

    Primeiro commit do Projeto de backend utilizando SpringBoot

---
+++ src/main/java/senacdw20232eliezerbackend/exemplodw/ExemploDwApplication.java
@@ -0,0 +1,13 @@
+package senacdw20232eliezerbackend.exemplodw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploDwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploDwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/senacdw20232eliezerbackend/exemplodw/ExemploDwApplicationTests.java
@@ -0,0 +1,13 @@
+package senacdw20232eliezerbackend.exemplodw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploDwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
