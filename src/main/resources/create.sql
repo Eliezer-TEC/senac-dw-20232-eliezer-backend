d52e9dfa109b0ba42cfdc1dcabddf34760a9a1c2
Eliezer Silva <126704018+Eliezer-TEC@users.noreply.github.com>
2023-09-19 19:31:48 -0300

    Add files via upload

---
+++ src/main/resources/create.sql
@@ -0,0 +1,25 @@
+drop schema EXEMPLOS;
+
+CREATE SCHEMA EXEMPLOS;
+USE EXEMPLOS;
+
+CREATE TABLE exemplos.fabricantes (
+	id int auto_increment NOT NULL,
+	nome varchar(400) NOT NULL,
+	cnpj varchar(14) NOT NULL,
+    cidade varchar(400) NOT NULL,
+	cep varchar(9) NOT NULL,
+    uf varchar(2) NOT NULL,
+	CONSTRAINT fabricantes_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE exemplos.produtos (
+	id int auto_increment NOT NULL,
+	nome varchar(400) NOT NULL,
+	id_fabricante int NOT NULL,
+	valor DECIMAL NOT NULL,
+	peso DECIMAL NOT NULL,
+	data_cadastro DATE NOT NULL,
+	CONSTRAINT produtos_pk PRIMARY KEY (id),
+	CONSTRAINT id_fabricante foreign key (id_fabricante) references exemplos.fabricantes(ID)
+);
